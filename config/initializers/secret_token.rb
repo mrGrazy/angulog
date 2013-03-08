fd2735d21b614934c56bdb0dcd2ff2c9adf53c2f
Andy Gray <mr.andy.gray@gmail.com>
2013-03-09 09:58:58 +1300

    A new rails app with some gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AngularBlog::Application.config.secret_token = '73579c1c9f44008353e8ac2761e7a49b65510713cc7bb08ea4818b101de1485f79f555a69901b4a87af89c62fb2839dfee742f7c90a94674a332860bd55abb36'
